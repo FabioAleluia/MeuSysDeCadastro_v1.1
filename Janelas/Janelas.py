948bc581919610e93951959850cd60d8081c1b95
Fabio Aleluia <aleluia.mj@gmail.com>
2022-02-01 18:32:33 -0300

    18-32

--- Janelas/Janelas.py
+++ Janelas/Janelas.py
@@ -6,4 +6,6 @@ class GetJanelas:
         self.win_login = login=uic.loadUi("Janelas/Windows/login_win_one.ui")
         self.win_opcoes = opcoes=uic.loadUi("Janelas/Windows/opcoes_win_one.ui")
         self.win_criar_login = criar_login=uic.loadUi("Janelas/Windows/criar_login.ui") 
+        self.win_formulario_Pfisica = formulario_Pfisica=uic.loadUi("Janelas/Windows/formulario_P_fisica.ui")
+        self.win_formulario_Pjuridica = formulario_Pjuridica=uic.loadUi("Janelas/Windows/formulario_P_juridica.ui")
         
--- Run.py
+++ Run.py
@@ -1,21 +1,47 @@
 from PyQt5 import uic,QtWidgets
 from Janelas import Janelas
 
+
 def FazerLogin():
     welcome.close()
     tela_login.show()
 
-def VoltaWelcome():
+def VoltaWelcome(): # Sai da Tela de "Login" e volta pra Tela de "Bem-Vindo"
     tela_login.close()
     welcome.show()
 
-def CriarLogin(): # Volta da tela de "Login" para tela de Bem-Vindo
+def CriarLogin(): # Sai da Tela "Bem-Vindo" e vai pra tela de "Criar Login"
     welcome.close()
     criar_login.show()
 
 def clforwelcome(): # Volta da Tela "Criar Login" para tela de Bem-Vindo
     criar_login.close()
     welcome.show()
+
+def LoginOpcoes():
+    tela_login.close()
+    tela_opcoes.show()
+
+def OpcoesForLogin():
+    tela_opcoes.close()
+    tela_login.show()
+
+def CadastroFisico():
+    tela_opcoes.close()
+    formulario_Pfisica.show()
+
+
+def CadastroJuridico():
+    tela_opcoes.close()
+    formulario_Pjuridica.show()
+
+def VoltaParaOpcoes_1():
+    formulario_Pjuridica.close()
+    tela_opcoes.show()
+
+def VoltaParaOpcoes_2():
+    formulario_Pfisica.close()
+    tela_opcoes.show()
 
 app=QtWidgets.QApplication([])
 
@@ -26,13 +52,20 @@ welcome = win.welcome
 tela_login = win.win_login
 criar_login = win.win_criar_login
 tela_opcoes = win.win_opcoes
-
+formulario_Pfisica = win.win_formulario_Pfisica
+formulario_Pjuridica = win.win_formulario_Pjuridica
 
 # Ações dos Botões
 welcome.fazer_login.clicked.connect(FazerLogin)
 welcome.criar_login.clicked.connect(CriarLogin)
 tela_login.volta.clicked.connect(VoltaWelcome)
+tela_login.login.clicked.connect(LoginOpcoes)
 criar_login.volta.clicked.connect(clforwelcome)
+tela_opcoes.logoff.clicked.connect(OpcoesForLogin)
+tela_opcoes.p_juridica.clicked.connect(CadastroJuridico)
+tela_opcoes.p_fisica.clicked.connect(CadastroFisico)
+formulario_Pjuridica.volta.clicked.connect(VoltaParaOpcoes_1)
+formulario_Pfisica.volta.clicked.connect(VoltaParaOpcoes_2)
 
 welcome.show()
 app.exec()
